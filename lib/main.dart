8f7a60c4454188c833768113c3023819c3c35413
jec-ode93 <jeanemmanuelcadet@gmail.com>
2021-07-07 16:35:50 -0400

    A basic authentication Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/screens/auth_screen.dart';
 import 'package:flutter/material.dart';
 import 'screens/chat_screen.dart';
 
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: ChatScreen(),
+      home: AuthScreen(),
     );
   }
 }
