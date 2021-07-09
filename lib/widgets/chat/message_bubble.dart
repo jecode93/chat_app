14cab5104b280a119ae365b1042050de067ed21a
jec-ode93 <jeanemmanuelcadet@gmail.com>
2021-07-09 00:12:39 -0400

    Displaying User Images

--- lib/widgets/chat/message_bubble.dart
+++ lib/widgets/chat/message_bubble.dart
@@ -4,55 +4,79 @@ import 'package:flutter/material.dart';
 class MessageBubble extends StatelessWidget {
   final String message;
   final String userName;
+  final String userImage;
   final bool isMe;
   final Key key;
-  const MessageBubble(this.message, this.isMe, this.userName,
+  const MessageBubble(this.message, this.isMe, this.userName, this.userImage,
       {required this.key});
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+    return Stack(
       children: [
-        Container(
-          decoration: BoxDecoration(
-            color: isMe ? Colors.grey[300] : Theme.of(context).accentColor,
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(12),
-              topRight: Radius.circular(12),
-              bottomLeft: !isMe ? Radius.circular(0) : Radius.circular(12),
-              bottomRight: !isMe ? Radius.circular(0) : Radius.circular(12),
+        Row(
+          mainAxisAlignment:
+              isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: isMe ? Colors.grey[300] : Theme.of(context).accentColor,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(12),
+                  topRight: Radius.circular(12),
+                  bottomLeft: !isMe ? Radius.circular(0) : Radius.circular(12),
+                  bottomRight: !isMe ? Radius.circular(0) : Radius.circular(12),
+                ),
+              ),
+              width: 140,
+              padding: EdgeInsets.symmetric(
+                vertical: 10,
+                horizontal: 16,
+              ),
+              margin: EdgeInsets.symmetric(
+                vertical: 16,
+                horizontal: 8,
+              ),
+              child: Column(
+                crossAxisAlignment:
+                    isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    userName,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: isMe
+                          ? Colors.black
+                          : Theme.of(context).textTheme.headline1!.color,
+                    ),
+                  ),
+                  Text(
+                    message,
+                    textAlign: isMe ? TextAlign.end : TextAlign.start,
+                    style: TextStyle(
+                      color: isMe
+                          ? Colors.black
+                          : Theme.of(context).textTheme.headline1!.color,
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-          width: 140,
-          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
-          margin: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-          child: Column(
-            crossAxisAlignment:
-                isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
-            children: [
-              Text(
-                userName,
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: isMe
-                      ? Colors.black
-                      : Theme.of(context).textTheme.headline1!.color,
-                ),
-              ),
-              Text(
-                message,
-                textAlign: isMe ? TextAlign.end : TextAlign.start,
-                style: TextStyle(
-                  color: isMe
-                      ? Colors.black
-                      : Theme.of(context).textTheme.headline1!.color,
-                ),
-              ),
-            ],
+          ],
+        ),
+        Positioned(
+          top: 0,
+          left: isMe ? null : 120,
+          right: isMe ? 120 : null,
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+              userImage,
+            ),
           ),
         ),
       ],
+      // ignore: deprecated_member_use
+      clipBehavior: Clip.hardEdge,
     );
   }
 }
